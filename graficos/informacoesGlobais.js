9fcd5e3c841908bdcab3cf0b54163a9a1ba99bcb
guilhermeaugusto <110115887+meclovin22@users.noreply.github.com>
2024-10-04 08:47:15 -0300

    Add files via upload

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,20 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+console.log(url);
+
+async function visualizaDadosGlobais(){
+    const res = await fetch(url);
+    const dados = await res.json();
+    const totalPessoasMundo =  (dados.total_pessoas_mundo)/1e9
+    const totalPessoasConectadas =  (dados.total_pessoas_conectadas)/1e9
+    const tempoMedio = dados.tempo_medio;
+
+    console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-texto');
+    const container = document.getElementById('graficos-container');
+    paragrafo.innerHTML = `O mundo tem  <span> ${totalPessoasMundo} bilhões </span> de habitantes, dos quais <span> ${totalPessoasConectadas} estão conectados em alguma rede social por um tempo médio de <span> ${tempoMedio} </span>`
+    container.appendChild(paragrafo)
+}
+
+visualizaDadosGlobais();
