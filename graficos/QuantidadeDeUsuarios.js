8d30f215c9d567ee01f9eac430b40c1343cd9020
guilhermeaugusto <110115887+meclovin22@users.noreply.github.com>
2024-10-11 08:54:04 -0300

    Add files via upload

--- graficos/QuantidadeDeUsuarios.js
+++ graficos/QuantidadeDeUsuarios.js
@@ -2,14 +2,20 @@
 
 async function QuantidadeDeUsuarios(){
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
+    const res = await fetch(url);
     const dados = await res.json();
     const nomeDasRedes = Object.keys(dados);
-    const QuantidadeDeUsuarios = Object.values(dados)
+    const QuantidadeDeUsuarios = Object.values(dados);
+
     const infos = [{
         x:nomeDasRedes,
         y:QuantidadeDeUsuarios,
         type:'bar'
     }]
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    Plotly.newPlot(grafico,infos);  
 
 }
+QuantidadeDeUsuarios();
